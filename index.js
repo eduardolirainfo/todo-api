8f4e3a88344a47a7f4e5543fb9c3be1395e334dd
eduardolirainfo <eduardolirainfo@gmail.com>
2021-02-17 22:20:09 -0300

    add todos list and create promises

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from "express"
+import { promises as fs } from "fs"
 
+const { readFile } = fs
 const app = express()
+app.use(express.json())
 
-app.listen('8080', ()=>console.log("API Start"))
+
+app.get("/todo", async(req, res) =>{
+    const data = JSON.parse(await readFile("todos.json"))
+    res.send(data);
+})
+
+app.listen(8080, ()=>console.log("API Started"))
+
+
+
