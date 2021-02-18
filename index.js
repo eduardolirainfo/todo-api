48f94ccd80232623f03e6d717072cbfa2cf32ea2
eduardolirainfo <eduardolirainfo@gmail.com>
2021-02-17 22:46:25 -0300

    get, post and delete

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 import express from "express"
 import { promises as fs } from "fs"
+import cors from "cors"
 
-const { readFile } = fs
+const { readFile, writeFile } = fs
+const fileName = "todos.json"
 const app = express()
+
 app.use(express.json())
+app.use(cors())
 
 
-app.get("/todo", async(req, res) =>{
-    const data = JSON.parse(await readFile("todos.json"))
-    res.send(data);
+app.post("/todo",async(req, res) => {
+    try {
+        if(req.body.description){
+            const data = JSON.parse(await readFile(fileName))
+            const todo = {
+                id: data.nextId++,
+                description: req.body.description
+            }
+            data.todos.push(todo)
+            await writeFile(fileName, JSON.stringify(data))
+            res.send(data)
+        }else{
+            res.status(400).send({error: "Descrição não informada!"})
+        }
+    }catch (err){
+        res.status(400).send({error: err.message})
+    }
+})
+
+
+app.get("/todo", async (req, res) =>{
+    try {
+        const data = JSON.parse(await readFile(fileName))
+        res.send(data)
+    }catch (err){
+       res.status(400).send({error: err.message})
+    }
+})
+
+app.delete("/todo/:id", async (req, res) => {
+    try {
+        if(req.params.id){
+            const data = JSON.parse(await readFile(fileName))
+            data.todos = data.todos.filter(
+                todos => todos.id !== parseInt(req.params.id))
+                await writeFile(fileName, JSON.stringify(data))
+                res.send(data)
+        }else{
+            res.status(400).send({error: "Id não informado!"})
+        }
+    }catch (err){
+        res.status(400).send({error: err.message})
+    }
 })
 
 app.listen(8080, ()=>console.log("API Started"))
